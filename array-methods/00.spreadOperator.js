724778367de8b7c2caa4e1f1e604a44705bbc2bb
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-31 18:47:13 -0500

    completar explicación de spread operator

--- array-methods/00.spreadOperator.js
+++ array-methods/00.spreadOperator.js
@@ -48,16 +48,57 @@ const user = {
 
 const userContact = {
     address: 'Mansion McPato',
+    phone: {
+        main: '+57 555-555-5555',
+        home: '+57 666-666-6666',
+        office: '+57 777-777-7777'
+    },
     email: 'hugo_ma_pato@tiorico.com'
 }
 
-const fullUserConcat = Object.assign({}, user, userContact);
-console.log(fullUserConcat)
+// const fullUserConcat = Object.assign({}, user, userContact);
+// console.log(fullUserConcat)
 
 const fullUserSpread = {
     ...user,
     ...userContact,
+    age: 8,
     email: 'hugo-ma-pato1@tiorico.com'
 }
+// console.log(fullUserSpread)
+
+// Nota: tener cuidado con la creación de los objetos ya que solo copia el primer nivel de profundidad, si existen más niveles de profundidad dentro del objeto, los objetos no se copian si no que se crea una referencia al espacio en memoria del objeto original.
+
+//ejemplo modificando una propiedad de primer nivel de profundidad en el objeto userContact
+userContact.address = '🏰 McPato'
+
+userContact.phone.home = '☎'
+userContact.phone.office = '📵'
+
+console.log(userContact)
 console.log(fullUserSpread)
 
+
+
+// uso 5. distribuir los elementos de un arreglo en los parámetros de una función
+
+function sum(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+const numbersForFunction = [5, 4, 11]
+
+const result = sum(numbersForFunction[0], numbersForFunction[1], numbersForFunction[2])
+const resultSpread = sum(...numbersForFunction)
+
+console.log(result, resultSpread)
+
+
+// uso 6. El anterior caso habre la puerta a la operación inversa REST operator o el resto de los elementos
+function sumTwoAndOthers(num1, num2, ...others) {
+    console.log(`El primer número: ${num1}`)
+    console.log(`El segundo número: ${num2}`)
+    console.log(`El resto de los numeros enviados: ${others}`)
+}
+
+sumTwoAndOthers(5, 8, 1, 2, 3, 4)
